e13cfd7e5a2cc1146990742fe44db2332eb42efe
Adil Feroze <adilferoze321@gmail.com>
2024-02-04 23:02:43 +0500

    outputs, comments, alerts, and id-selectors

---
+++ basics/script.js
@@ -0,0 +1,13 @@
+// console.log(`Hello`);
+// console.log('World');
+// window.alert("This is an alert!");
+// window.alert("I like nuggets :>");
+
+// single line comment
+
+/*
+multi line comment
+*/
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = "I AM BATMAN";

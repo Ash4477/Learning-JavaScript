7b1d51ee0874517a648c919826e26357fc28ec55
Adil Feroze <adilferoze321@gmail.com>
2024-02-04 23:19:24 +0500

    variable,data-types, typeof

--- basics/script.js
+++ basics/script.js
@@ -9,5 +9,36 @@
 multi line comment
 */
 
-document.getElementById("myH1").textContent = "Hello";
-document.getElementById("myP").textContent = "I AM BATMAN";
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = "I AM BATMAN";
+
+// let x;
+// x = 100;
+// console.log(x);
+
+// let y=234;
+// console.log(y);
+
+// let age = 22;
+// let price = 10;
+
+// console.log(`I am ${age} years old`);
+// console.log(`The price is $${price}.`);
+
+// console.log(typeof age);
+
+// let firstName = "Ash";
+// console.log(typeof firstName);
+
+// let online = true;
+// let offline = false;
+
+// console.log(typeof online);
+
+let name = "Ash F";
+let isStudent = true;
+let age = 22;
+
+document.getElementById("p1").textContent = `Name: ${name}`;
+document.getElementById("p2").textContent = `Age: ${age}`;
+document.getElementById("p3").textContent = `isStudent: ${isStudent}`;

f9575c50c71cde5b8b2965e76744e42e6085885e
Adil Feroze <adilferoze321@gmail.com>
2024-02-08 23:59:37 +0500

    Random Num Generator

--- random-number-generator/randNum.js
+++ random-number-generator/randNum.js
@@ -1,6 +1,10 @@
-let min = 1;
-let max = 6;
+
 
 let randomNum = Math.floor(Math.random()* (max-min)) + min;
 
 console.log(randomNum);
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
